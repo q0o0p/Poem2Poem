787dc7cf8c4017cade6839cceef993b75d657fd5
Anna Klepova <q0o0p@yandex.ru>
2019-02-22 15:26:45 +0300

    Implement train file tokenization and token list creation in 'main.py'

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import getopt
+import numpy as np
+from collections import Counter
 
 
 
@@ -75,3 +77,39 @@ with open(train_file, encoding = 'utf-8') as f:
         train_text_pairs.append(text_pair)
 
 print(' {} text pairs read.'.format(len(train_text_pairs)))
+
+
+# Tokenize train file
+# --------------------
+
+print('Tokenizing train file...')
+
+train_toks_pairs = tuple(tuple(tuple(t.split())
+                               for t in text_pair)
+                         for text_pair in train_text_pairs)
+del train_text_pairs
+
+tok_ctr_pair = (Counter(), Counter())
+for toks_pair in train_toks_pairs:
+    for tok_ctr, toks in zip(tok_ctr_pair, toks_pair):
+        tok_ctr.update(toks)
+
+for title, tok_ctr in zip(['source', 'target'], tok_ctr_pair):
+    print(' {} unique tokens: {}'.format(title, len(tok_ctr)))
+
+special_toks = ('<BOS>', '<EOS>')
+bos_tok_id, eos_tok_id = 0, 1
+
+SOURCE_PAIR_IDX, TARGET_PAIR_IDX = 0, 1
+
+tok_list_pair = tuple(special_toks + tuple(tok_ctr.keys())
+                      for tok_ctr in tok_ctr_pair)
+
+tok_to_id_pair = tuple({ tok: idx + len(special_toks) for idx, tok
+                         in enumerate(tok_list[len(special_toks):]) }
+                       for tok_list in tok_list_pair)
+
+train_tok_ids_pairs = np.array([tuple(np.array([tok_to_id[tok] for tok in toks], dtype = np.int32)
+                                      for tok_to_id, toks in zip(tok_to_id_pair, toks_pair))
+                                for toks_pair in train_toks_pairs], dtype = object)
+del train_toks_pairs
